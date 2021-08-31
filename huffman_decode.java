9dc77ac51fa6fd118e235cb4ff640a8919fec103
calvin <alexcarll@yahoo.com>
2021-08-31 16:08:14 -0700

    Add files via upload
    file upload from this morning

---
+++ huffman_decode.java
@@ -0,0 +1,16 @@
+//hackerrank Huffman decode
+
+void decode(String S, Node root)
+{
+    StringBuilder sb = new StringBuilder();
+    Node c = root;
+    for (int i = 0; i < S.length(); i++) {
+        c = S.charAt(i) == '1' ? c.right : c.left;
+        if (c.left == null && c.right == null) {
+            sb.append(c.data);
+            c = root;
+        }
+    }
+    System.out.print(sb);
+}
+
